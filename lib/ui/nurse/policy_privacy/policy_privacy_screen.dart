d221f5d84784dd6c20d66d0f228c92301bcec646
Salma Elbasha <m.elgamal0551@gmail.com>
2025-03-11 03:23:31 +0200

    update

---
+++ lib/ui/nurse/policy_privacy/policy_privacy_screen.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:intl/intl.dart';
+
+class PatientDataScreen extends StatefulWidget {
+  @override
+  _PatientDataScreenState createState() => _PatientDataScreenState();
+}
+
+class _PatientDataScreenState extends State<PatientDataScreen> {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leadingWidth: 150.w, // توسيع المساحة الخاصة بـ leading
+        leading: Padding(
+          padding: EdgeInsets.only(left: 16.w), // دفع المحتوى ناحية اليمين
+          child: Row(
+            children: [
+              GestureDetector(
+                onTap: () {
+                  Navigator.pop(context);
+                },
+                child: Container(
+                  width: 24.w,
+                  height: 24.h,
+                  decoration: BoxDecoration(
+                    color: Color(0xffB93439),
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  alignment: Alignment.center, // لضبط الأيقونة في المنتصف
+                  child: Icon(
+                    Icons.arrow_back_ios_new_rounded,
+                    color: Colors.white,
+                    size: 16,
+                  ),
+                ),
+              ),
+              SizedBox(width: 5.w), // توسيع المسافة بين الأيقونة والنص
+              Text(
+                "PolicyPrivacy",
+                style: TextStyle(
+                  fontSize: 18.sp,
+                  fontWeight: FontWeight.w600,
+                  color: Color(0xffB93439),
+                ),
+              ),
+            ],
+          ),
+        ),
+        automaticallyImplyLeading: false,
+        elevation: 0,
+        backgroundColor: Colors.white,
+      ),
+
+      body: Column()
+    );
+  }
+
+
+}
